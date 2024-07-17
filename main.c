c7e21eef6ef6af551deb9bde2e3e5ae30b00bc64
antoine turello <turello@et.esiea.fr>
2024-07-17 23:18:14 +0200

    print log lexer

--- main.c
+++ main.c
@@ -110,7 +110,7 @@ int main() {
     do {
         alphanum = lexer_getalphanum(&buffer);
         if (alphanum) {
-            printf("Séquence alphanumérique trouvée : %s\n", alphanum);
+            fprintf(stderr, "Séquence alphanumérique trouvée : %s\n", alphanum);
             symbol_table_add(table, alphanum, VAR_TYPE_VOID);
             free(alphanum);
         }
@@ -118,7 +118,7 @@ int main() {
 
     char *op;
     while ((op = lexer_getop(&buffer)) != NULL) {
-        printf("Opérateur trouvé : %s\n", op);
+        fprintf(stderr, "Opérateur trouvé : %s\n", op);
         free(op);
     }
 
@@ -126,7 +126,7 @@ int main() {
     while (true) {
         number = lexer_getnumber(&buffer);
         if (number != -1) {
-            printf("Number found: %ld\n", number);
+            fprintf(stderr, "Number found: %ld\n", number);
         } else {
             break; // Exit loop if no more numbers are found
         }
